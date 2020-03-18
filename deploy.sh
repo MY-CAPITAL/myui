0b53a88c89e8b4ae6598c739bc189a6e0aca16bc
huangyuesong <ph19779@gmail.com>
2020-03-18 15:05:40 +0800

    Remove "deploy*.sh"

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-scp -r dist rss@192.168.1.14:~/bss_fe/node_modules/lovue
-scp -r dist rss@192.168.1.14:~/bss_fe_mobile/node_modules/lovue
-scp -r dist rss@192.168.1.14:~/bss_fe_otcam/node_modules/lovue
--- deploy_local.sh
+++
@@ -1,3 +0,0 @@
-scp -r dist ../bss_fe/node_modules/lovue
-scp -r dist ../bss_fe_mobile/node_modules/lovue
-scp -r dist ../bss_fe_otcam/node_modules/lovue
